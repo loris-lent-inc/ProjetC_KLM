452932ddc4a8fa64d091baff081f52ffd0a280a0
loris_lent <62310830+le-loris@users.noreply.github.com>
2023-03-09 15:00:12 +0100

    test commit

--- Source.c
+++ Source.c
@@ -1,4 +1,5 @@
-
+#include "libCIPSI1.h"
 void main() {
-
+    IMAGE img1 = lectureImage("Ground truth/In_1.pgm");
+    printf("%d", img1.Nbcol);
 }
--- libCIPSI1.h
+++ libCIPSI1.h
@@ -1,4 +1,5 @@
 #pragma once
+#include <stdio.h>
 //#include "libOutils.h"
 
 /* librairie IPSI1 */
